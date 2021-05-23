a41811a0d088ef2d69c361408cc3101743276d7b
Alberto Fustinoni <alberto@aftnet.net>
2021-05-23 12:28:06 +0900

    Logic

--- src/cpub_creator/mod.rs
+++ src/cpub_creator/mod.rs
@@ -1,3 +1,25 @@
+struct Metadata {
+    title: String,
+    author: String,
+}
+
+impl Default for Metadata {
+    fn default() -> Self {
+        Metadata {
+            title: String::from("Some Title"),
+            author: String::from("Some Author")
+        }
+    }
+}
+
+struct Writer {
+
+}
+
 pub fn lol() {
     println!("abc");
+    let mut test: Metadata = Default::default();
+    test.title = String::from("ab");
+    println!("{}", test.title);
+    println!("{}", test.author);
 }
