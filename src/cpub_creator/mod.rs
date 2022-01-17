45bf0d8b0f773a1bc71d2122592669f8625daf2f
Alberto Fustinoni <alberto@aftnet.net>
2022-01-17 17:59:41 +0900

    Logic

--- src/cpub_creator/mod.rs
+++ src/cpub_creator/mod.rs
@@ -1,16 +1,22 @@
 mod metadata;
 pub use metadata::Metadata;
 
+pub use zip::write::ZipWriter;	
+
 pub struct Writer {
     pub metadata: Metadata
 }
 
 impl Writer {
-    pub fn new(path: &str) -> Self {
-        let output = Writer {
-            metadata: Metadata::default()
+    pub fn new(path: &std::path::Path) -> std::io::Result<Writer> {
+        let mut file = match std::fs::File::create(path) {
+            Ok(res) => res,
+            Err(err) => return Err(err),
         };
-        return output;
+        
+        let mut writer = zip::ZipWriter::new(file);
+        let mut output : Writer;
+        return Ok(output);
     }
 
     pub fn set_cover() {
