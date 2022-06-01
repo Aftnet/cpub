2fa6c6e117fc997c4b5d3fe2e3ffb9fa4778e90f
Alberto Fustinoni <alberto@aftnet.net>
2022-06-01 18:13:36 +0900

    Logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::fs::File;
+use std::path::Path;
+
 mod cpub_creator;
 
 fn main() {
     println!("Opening");
-    let mut writer = cpub_creator::create_at(std::path::Path::new("D:\\Test.epub")).unwrap();
+    let mut writer = cpub_creator::create_at(Path::new("test.epub")).unwrap();
     for i in 0..4 {
         println!("Adding {}", i);
-        let mut file = std::fs::File::open(std::path::Path::new("C:\\Users\\Alberto\\Pictures\\Avatars\\Chuuya 01.png")).unwrap();
+        let mut file = File::open(Path::new("test/img01.png")).unwrap();
         writer.add_image(&mut file).unwrap();
     }
 }
