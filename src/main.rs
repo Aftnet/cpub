3662b962ff9d9f6bd5136b582c5da0eef136f9a7
Alberto Fustinoni <alberto@aftnet.net>
2022-06-13 19:24:45 +0900

    Logic

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,106 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 
-use clap::{App, Arg, SubCommand};
+use clap::{crate_authors, crate_version, Arg, Command};
 use cpub::{EpubWriter, Metadata};
 
 fn main() {
-    let matches = App::new("Comic ePub maker")
-        .version("1.0")
-        .author("Aftnet")
-        .about("Create ePubs from images")
-        .arg(
-            Arg::new("title")
-                .short('t')
-                .long("title")
-                .value_name("TITLE")
-                .help("Sets the title")
-                .takes_value(true)
-                .use_delimiter(false),
-        )
+    let common_args = vec![
+        Arg::new("title")
+            .short('t')
+            .long("title")
+            .value_name("TITLE")
+            .help("Sets the title")
+            .required(true)
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("author")
+            .short('a')
+            .long("author")
+            .value_name("AUTHOR")
+            .help("Sets the author")
+            .required(true)
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("publisher")
+            .short('p')
+            .long("publisher")
+            .value_name("PUBLISHER")
+            .help("Sets the publisher")
+            .required(true)
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("published-date")
+            .short('d')
+            .long("published-date")
+            .value_name("PUBLISHED-DATE")
+            .help("Sets the date of publication")
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("language")
+            .long("language")
+            .value_name("LANGUAGE")
+            .help("Sets the language")
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("description")
+            .long("description")
+            .value_name("DESCRIPTION")
+            .help("Sets the description")
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("source")
+            .long("source")
+            .value_name("SOURCE")
+            .help("Sets the source")
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("copyright")
+            .long("copyright")
+            .value_name("COPYRIGHT")
+            .help("Sets the copyright")
+            .takes_value(true)
+            .multiple_values(false),
+        Arg::new("rtl")
+            .short('r')
+            .long("rtl")
+            .value_name("RTL")
+            .help("Sets the reading order as right to left (manga)")
+            .takes_value(false),
+        Arg::new("tags")
+            .long("tags")
+            .value_name("TAGS")
+            .help("Sets custom tags")
+            .takes_value(true)
+            .use_value_delimiter(true),
+    ];
+
+    let matches = Command::new("Comic ePub maker")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Create single ePub from images in a directory")
+        .args(&common_args)
         .subcommand(
-            App::new("batch")
-                .about("controls testing features")
-                .version("1.3")
-                .author("Someone E. <someone_else@other.com>")
-                .arg(
-                    Arg::new("debug")
-                        .short('d')
-                        .help("print debug information verbosely"),
-                ),
+            Command::new("batch")
+                .version(crate_version!())
+                .author(crate_authors!())
+                .about(
+                    "Create multiple ePubs from directory containig other directories with images",
+                )
+                .args(&common_args),
         )
         .get_matches();
 
-    if let Some(d) = matches.subcommand_matches("batch") {
+    if let Some(d) = matches.subcommand() {
         println!("Batch");
+        if let Some(e) = d.1.value_of("title") {
+            println!("Title {}", e);
+        }
     } else {
         println!("Non batch");
-    }
-
-    if let Some(d) = matches.value_of("title") {
-        println!("Title {}", d);
+        if let Some(e) = matches.value_of("title") {
+            println!("Title {}", e);
+        }
     }
     /*
     println!("Opening");
